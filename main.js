842c65d6ec4a14e3330282272b16bfc1dcdb36aa
iamaworld <sneakyclood@gmail.com>
2023-05-22 18:51:12 +0500

    Co-authored-by: Daniel <polukarp@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,20 +1,54 @@
+const choices = ['Rock', 'Paper', 'Scissors'];
+
 function getChoiceComputer() {
 	let choice = Math.floor(Math.random() * 3);
-	const choices = ['Rock', 'Paper', 'Scissors'];
 	return choices.at(choice);
 }
-console.log(getChoiceComputer());
-function playRound(playerSelection, computerSelection) {
+
+let playerSelection = '';
+let computerSelection = '';
+
+let rock = document.getElementById('Rock');
+let paper = document.getElementById('Paper');
+let scissors = document.getElementById('Scissors');
+rock.addEventListener('click', () => {
+	playerSelection = 'Rock';
+	computerSelection = getChoiceComputer();
 	if (playerSelection === 'Rock' && computerSelection === 'Paper') {
-		return 'You Lose! Paper beats Rock';
-	} else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
-		return 'You Lose! Rock beats Scissors';
-	} else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
-		return 'You Lose! Paper beats Rock';
+		console.log('You Lose! Paper beats Rock');
 	} else if (computerSelection === playerSelection) {
-		return 'Tie!';
+		console.log('Tie!');
+	} else if (!choices.includes(playerSelection)) {
+		console.log('Unknown value. Please try again');
 	} else {
-		return 'You win! ' + playerSelection + ' beats ' + computerSelection;
+		console.log('You win! ' + playerSelection + ' beats ' + computerSelection);
 	}
-}
-console.log(playRound('Rock', getChoiceComputer()));
+});
+
+paper.addEventListener('click', () => {
+	playerSelection = 'Paper';
+	computerSelection = getChoiceComputer();
+	if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+		console.log('You Lose! Paper beats Rock');
+	} else if (computerSelection === playerSelection) {
+		console.log('Tie!');
+	} else if (!choices.includes(playerSelection)) {
+		console.log('Unknown value. Please try again');
+	} else {
+		console.log('You win! ' + playerSelection + ' beats ' + computerSelection);
+	}
+});
+
+scissors.addEventListener('click', () => {
+	playerSelection = 'Scissors';
+	computerSelection = getChoiceComputer();
+	if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+		console.log('You Lose! Rock beats Scissors');
+	} else if (computerSelection === playerSelection) {
+		console.log('Tie!');
+	} else if (!choices.includes(playerSelection)) {
+		console.log('Unknown value. Please try again');
+	} else {
+		console.log('You win! ' + playerSelection + ' beats ' + computerSelection);
+	}
+});
